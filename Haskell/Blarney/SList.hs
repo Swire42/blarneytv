8134b860be88681dae1afdf9bf08033654558170
Victor Miquel <v_m_1732@orange.fr>
2024-04-08 12:48:14 +0200

    Enhanced type of SList

--- Haskell/Blarney/SList.hs
+++ Haskell/Blarney/SList.hs
@@ -23,6 +23,7 @@ module Blarney.SList (
 , Blarney.SList.toList
 
 , Blarney.SList.lazyShape
+, Blarney.SList.forceCast
 , Blarney.SList.replicate
 , Blarney.SList.singleton
 , Blarney.SList.append
@@ -55,20 +56,20 @@ import Data.Proxy
 import qualified Data.List as L
 
 data SList (n :: Nat) a where
-  Nil :: SList 0 a
-  Cons :: a -> SList n a -> SList (n+1) a
+  Nil :: (n ~ 0) => SList n a
+  Cons :: forall n a. (1 <= n) => a -> SList (n-1) a -> SList n a
 
 ifZero :: forall n a. KnownNat n => (n ~ 0 => a) -> (1 <= n => a) -> a
 ifZero a b = case (cmpNat @0 @n Proxy Proxy, cmpNat @1 @n Proxy Proxy) of
-    (EQI, GTI) -> a
-    (LTI, EQI) -> b
-    (LTI, LTI) -> b
-    _ -> undefined
+  (EQI, GTI) -> a
+  (LTI, EQI) -> b
+  (LTI, LTI) -> b
+  _ -> undefined
 
 ifEq :: forall a b v. (KnownNat a, KnownNat b) => (a ~ b => v) -> v -> v
 ifEq x y = case (cmpNat @a @b Proxy Proxy) of
-    EQI -> x
-    _ -> y
+  EQI -> x
+  _ -> y
 
 assertZero :: forall n v. KnownNat n => (n ~ 0 => v) -> v
 assertZero = assertEq @n @0
@@ -93,10 +94,13 @@ instance KnownNat n => Prelude.Foldable (SList n) where
   foldr = foldr
 
 instance KnownNat n => Prelude.Traversable (SList n) where
-  traverse f xss = ifZero @n (Prelude.pure Nil) (case xss of (Cons x xs) -> Prelude.pure Cons <*> f x <*> Prelude.traverse f xs)
+  traverse f xss = ifZero @n (Prelude.pure Nil) (let (Cons x xs) = xss in Prelude.pure Cons <*> f x <*> Prelude.traverse f xs)
 
 lazyShape :: forall n a. KnownNat n => SList n a -> SList n a
-lazyShape xss = ifZero @n Nil (case xss of Cons x xs -> Cons x xs)
+lazyShape xss = ifZero @n Nil (let Cons x xs = xss in Cons x xs)
+
+forceCast :: forall n m a. (KnownNat n, KnownNat m) => SList m a -> SList n a
+forceCast xs = ifEq @n @m xs (error "sizes don't match")
 
 replicate :: forall n a. KnownNat n => a -> SList n a
 replicate x = ifZero @n Nil (Cons x (replicate @(n-1) x))
@@ -105,13 +109,13 @@ singleton :: a -> SList 1 a
 singleton x = Cons x Nil
 
 append :: forall n m a. KnownNat n => SList n a -> SList m a -> SList (n+m) a
-append xss ys = ifZero @n ys (case xss of (Cons x xs) -> Cons x (append xs ys))
+append xss ys = ifZero @n ys (let (Cons x xs) = xss in Cons x (append xs ys))
 
 select :: forall i n a. (KnownNat i, KnownNat n, (i+1) <= n) => SList n a -> a
-select xs = ifZero @n undefined (case xs of (Cons y ys) -> ifZero @i y (select @(i-1) ys))
+select xs = ifZero @n undefined (let (Cons y ys) = xs in ifZero @i y (select @(i-1) ys))
 
-split :: forall n n0 n1 a. (KnownNat n, KnownNat n0, KnownNat n1, n0 ~ (n-n1)) => SList n a -> (SList n0 a, SList n1 a)
-split xs = ifZero @n0 (Nil, xs) ((case xs of (Cons h t) -> let (as :: SList (n0-1) a, bs :: SList n1 a) = split @(n-1) t in (Cons h as, bs)) :: (1 <= n) => (SList n0 a, SList n1 a))
+split :: forall n n0 n1 a. (KnownNat n, KnownNat n0, KnownNat n1, n1 ~ (n-n0), n0 <= n) => SList n a -> (SList n0 a, SList n1 a)
+split xs = ifZero @n0 (Nil, xs) ((let (Cons h t) = xs in let (as, bs) = split @(n-1) @(n0-1) @n1 t in (Cons h as, bs)) :: (1 <= n) => (SList n0 a, SList n1 a))
 
 update :: forall i n a. (KnownNat i, (i+1) <= n, 1 <= n) => (a -> a) -> SList n a -> SList n a
 update f (Cons x xs) = ifZero @i (Cons (f x) xs) (Cons x (update @(i-1) f xs))
@@ -126,16 +130,16 @@ tail :: (1 <= n) => SList n a -> SList (n-1) a
 tail (Cons _ xs) = xs
 
 init :: forall n a. (KnownNat n, 1 <= n) => SList n a -> SList (n-1) a
-init xs = ifZero @(n-1) Nil (case xs of Cons y ys -> Cons y (init ys))
+init xs = ifZero @(n-1) Nil (let Cons y ys = xs in Cons y (init ys))
 
 uncons :: (1 <= n) => SList n a -> (a, SList (n-1) a)
 uncons xs = (head xs, tail xs)
 
 take :: forall i n a. (KnownNat i, i <= n) => SList n a -> SList i a
-take xs = ifZero @i Nil ((case xs of (Cons h t) -> Cons h (take @(i-1) t)) :: (1 <= n) => SList i a)
+take xs = ifZero @i Nil ((let (Cons h t) = xs in Cons h (take @(i-1) t)) :: (1 <= n) => SList i a)
 
 drop :: forall n i a. (KnownNat n, KnownNat i, i <= n) => SList n a -> SList (n-i) a
-drop xs = ifZero @(i) xs ((case xs of (Cons _ t) -> (drop @(n-1) @(i-1) t :: (i-1 <= n-1) => SList (n-i) a)) :: (1 <= n) => SList (n-i) a)
+drop xs = ifZero @(i) xs ((let (Cons _ t) = xs in (drop @(n-1) @(i-1) t :: (i-1 <= n-1) => SList (n-i) a)) :: (1 <= n) => SList (n-i) a)
 
 rotateL :: forall n a. (KnownNat n, 1 <= n) => SList n a -> SList n a
 rotateL xs = (tail xs) `append` singleton (head xs)
@@ -147,19 +151,19 @@ reverse :: KnownNat n => SList n a -> SList n a
 reverse = aux Nil
   where
     aux :: forall n0 n1 n a. (KnownNat n0, KnownNat n1, n0+n1 ~ n) => SList n0 a -> SList n1 a -> SList n a
-    aux acc xss = ifZero @n1 acc (case xss of (Cons x xs) -> aux xs (Cons x acc))
+    aux acc xss = ifZero @n1 acc (let (Cons x xs) = xss in aux xs (Cons @(n0+1) x acc))
 
 zip :: forall n a b. KnownNat n => SList n a -> SList n b -> SList n (a, b)
-zip xss yss = ifZero @n Nil (case (xss, yss) of (Cons x xs, Cons y ys) -> Cons (x, y) (zip xs ys))
+zip xss yss = ifZero @n Nil (let (Cons x xs, Cons y ys) = (xss, yss) in Cons (x, y) (zip xs ys))
 
 unzip :: forall n a b. KnownNat n => SList n (a, b) -> (SList n a, SList n b)
-unzip xyss = ifZero @n (Nil, Nil) (case xyss of (Cons (x, y) xys) -> let (xs, ys) = unzip xys in (Cons x xs, Cons y ys))
+unzip xyss = ifZero @n (Nil, Nil) (let (Cons (x, y) xys) = xyss in let (xs, ys) = unzip xys in (Cons x xs, Cons y ys))
 
 map :: forall n a b. KnownNat n => (a -> b) -> SList n a -> SList n b
-map f xss = ifZero @n Nil (case xss of (Cons x xs) -> Cons (f x) (map f xs))
+map f xss = ifZero @n Nil (let (Cons x xs) = xss in Cons (f x) (map f xs))
 
 foldr :: forall n a b. KnownNat n => (a -> b -> b) -> b -> SList n a -> b
-foldr f e xss = ifZero @n e (case xss of (Cons x xs) -> x `f` foldr f e xs)
+foldr f e xss = ifZero @n e (let (Cons x xs) = xss in x `f` foldr f e xs)
 
 transpose :: forall m n a. (KnownNat n, KnownNat m) => SList m (SList n a) -> SList n (SList m a)
 transpose x = ifZero @n Nil (let (y, ys) = unzip . map (uncons) $ x in Cons y (transpose ys))
